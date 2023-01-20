64d71ac70655c801313e75edd38aa82492b44117
Isaac Boussum <isaacboussum10@gmail.com>
2023-01-19 18:41:43 -0700

    problem 9 and 10 done

--- lab.js
+++ lab.js
@@ -135,16 +135,41 @@ while (score < 7) {
 
 //CODE HERE
 
+let changeMyMind = true
+
+if (changeMyMind) {
+  changeMyMind = false
+  console.log ('changed to false')
+} else {
+  changeMyMind = true
+  console.log ('it is true')
+}
+
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, 
+console.log it's new value (it should now read true).
 
 //CODE HERE
+
+let changeMyMind = true
+
+if (changeMyMind) {
+  changeMyMind = false
+  console.log ('changed to false')
+} else {
+  changeMyMind = true
+  console.log ('it is true')
+}
+
+changeMyMind = !changeMyMind
+console.log ('it is now true again')
 
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
 
 let z = 5;
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. 
+If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
